b6dc3a0b56ed8d49db68ea500dd24cf50ffb5ad3
Christian Bromann <git@bromann.dev>
2022-09-26 20:31:36 -0700

    add ability to cancel execution

--- src/extension/executors/shell.ts
+++ src/extension/executors/shell.ts
@@ -1,5 +1,5 @@
 import path from 'node:path'
-import { writeFile } from 'node:fs/promises'
+import { writeFile, chmod } from 'node:fs/promises'
 import { spawn } from 'node:child_process'
 import type EventEmitter from 'node:events'
 
@@ -18,10 +18,13 @@ async function shellExecutor(
   const outputItems: string[] = []
   const scriptFile = await file()
   await writeFile(scriptFile.path, doc.getText(), 'utf-8')
+  await chmod(scriptFile.path, 0o775)
 
-  const child = spawn('sh', [scriptFile.path], {
-    cwd: path.dirname(doc.uri.path)
+  const child = spawn(scriptFile.path, {
+    cwd: path.dirname(doc.uri.path),
+    shell: true
   })
+  console.log(`[RunMe] Started process on pid ${child.pid}`)
 
   inputHandler.on('data', (input) => {
     child.stdin.write(`${input}\n`)
@@ -46,7 +49,25 @@ async function shellExecutor(
 
   child.stdout.on('data', handleOutput)
   child.stderr.on('data', handleOutput)
-  return !Boolean(await new Promise((resolve) => child.on('exit', resolve)))
+  return !Boolean(await new Promise<number>((resolve) => {
+    /**
+     * register cancellation handler
+     * ToDo(Christian): maybe better to kill with SIGINT signal but that doesn't stop the
+     * prcoess afterall
+     */
+    exec.token.onCancellationRequested(() => {
+      child.stdin.destroy()
+      child.stdout.off('data', handleOutput)
+      child.stderr.off('data', handleOutput)
+
+      if (child.pid) {
+        process.kill(child.pid, 'SIGHUP')
+      }
+      resolve(child.kill('SIGHUP') ? 0 : 1)
+    })
+
+    child.on('exit', resolve)
+  }))
 }
 
 export const sh = shellExecutor
