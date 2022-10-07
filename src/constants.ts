02389edc10b8f15745ebc7aa446da1314307f561
Christian Bromann <git@bromann.dev>
2022-10-06 18:02:53 -0700

    prepopulate PATH

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,10 @@ export const CONFIGURATION_SHELL_DEFAULTS = {
   closeTerminalOnSuccess: true
 } as const
 
-export const DEFAULT_ENV = { RUNME_TASK: 'true' }
+export const DEFAULT_ENV = {
+  RUNME_TASK: 'true',
+  PATH: process.env.PATH || ''
+}
 export const ENV_STORE = new Map<string, string>(
   Object.entries(DEFAULT_ENV)
 )
