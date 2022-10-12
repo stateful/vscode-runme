783dd97af552a84fbbdd84c4d3f0f80f12e60b02
Christian Bromann <git@bromann.dev>
2022-10-12 16:44:17 -0700

    use project vite.conf if existing

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,7 @@
-import { createServer, ViteDevServer } from 'vite'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
+import { createServer, ViteDevServer, InlineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
@@ -18,47 +21,29 @@ export class ViteServer {
     return this.#server.config.server.port
   }
 
-  static async start(argv: string[]) {
-    const yargv = await yargs(hideBin(argv)).options({
-      port: { type: 'number', required: true },
-      rootPath: { type: 'string', required: true }
-    }).argv
-
+  static async getStaticConfig(port: number, rootPath: string) {
     process.env.FAST_REFRESH = 'false'
-    const server = await createServer({
-      // any valid user config options, plus `mode` and `configFile`
-      configFile: false,
-      root: yargv.rootPath,
-      server: { port: yargv.port },
-      /**
-       * Currently we inject a TailwindCSS file with all static classes
-       * resulting in adding a 5mb assets. There is more work needed
-       * to ensure TW can be setup for a certain root directory that
-       * runs on Fresh.
-       */
-      // css: {
-      //   postcss: {
-      //     plugins: [
-      //       /**
-      //        * ToDo(Christian): make this optional based on directory structure
-      //        * Also this needs special setup for Deno apps which have their own
-      //        * integration.
-      //        */
-      //       tw({
-      //         config: {
-      //           selfURL: yargv.rootPath,
-      //           content: ['./**/*.html'],
-      //           safelist: [{
-      //             pattern: /.*/,
-      //             variants: []
-      //           }]
-      //         }
-      //       })
-      //     ],
-      //   }
-      // },
+
+    const config: InlineConfig = {
+      root: rootPath,
+      server: { port },
       plugins: [
-        cellResult({ projectRoot: yargv.rootPath }),
+        cellResult({ projectRoot: rootPath })
+      ]
+    }
+
+    const projectViteConfigPathJS = path.resolve(rootPath, 'vite.config.js')
+    const projectViteConfigPathTS = path.resolve(rootPath, 'vite.config.ts')
+    const hasViteConfig: string | false = await Promise.any([
+      fs.access(projectViteConfigPathJS).then(() => projectViteConfigPathJS),
+      fs.access(projectViteConfigPathTS).then(() => projectViteConfigPathTS)
+    ]).catch(() => false)
+    if (hasViteConfig) {
+      console.log(`[Runme] project ${hasViteConfig} found`)
+      config.configFile = hasViteConfig
+    } else {
+      console.log(`[Runme] no project ${hasViteConfig} found, using custom setup`)
+      config.plugins?.push(
         vue(),
         svelte({
           hot: false
@@ -66,9 +51,20 @@ export class ViteServer {
         react({
           fastRefresh: false
         })
-        // preact()
-      ]
-    })
+      )
+    }
+
+    return config
+  }
+
+  static async start(argv: string[]) {
+    const yargv = await yargs(hideBin(argv)).options({
+      port: { type: 'number', required: true },
+      rootPath: { type: 'string', required: true }
+    }).argv
+
+    const config = await ViteServer.getStaticConfig(yargv.port, yargv.rootPath)
+    const server = await createServer(config)
 
     await server.listen()
     console.log(`[Runme] Kernel server started successfuly on port ${yargv.port}, root path: ${yargv.rootPath}`)
