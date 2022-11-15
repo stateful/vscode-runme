f694036deefe37db9edfde79473fd7ce15a4b918
Lorenze Jay <lorenzejaytech@gmail.com>
2022-11-15 08:29:59 -0800

    added test to check for activate func registered commands check

--- tests/extension/extension.test.ts
+++ tests/extension/extension.test.ts
@@ -1,9 +1,15 @@
 import { test, expect, vi } from 'vitest'
 import { notebooks, workspace, commands } from 'vscode'
 
-import { RunmeExtension } from '../../src/extension/extension'
+import { RunmeExtension, activate } from '../../src/extension/extension'
 
 vi.mock('vscode')
+
+test('activate extension', () => {
+  const context: any = { subscriptions: [], extensionUri: { fsPath: '/foo/bar' } }
+  activate(context)
+  expect(commands.registerCommand).toBeCalledTimes(8) //2 from activate and 6 from RunmeExtension class intialise
+})
 
 test('initialises all providers', async () => {
   const context: any = { subscriptions: [], extensionUri: { fsPath: '/foo/bar' } }
