fb54c23f5211aa38a27acc66e75b6e241c5a2569
Lorenze Jay Hernandez <63378463+lorenzejay@users.noreply.github.com>
2022-11-16 09:38:26 -0800

    options to open runme as text editor and text-edit md as runme (#58)
    * options to open runme as text editor and text-edit md as runme
    
    * adjusted test as new commands registered total to 6
    
    * added test to check for activate func registered commands check
    
    * fixed test
    
    * more vs code terminology for editor nav button titles
    
    * updated wordings, tests made for commands and hiding commands for opening with text editor
    
    * new line bottom of file
    
    * passing tests
    
    * remove openSplitViewAsMarkdownText from command pallete

--- tests/extension/commands/index.test.ts
+++ tests/extension/commands/index.test.ts
@@ -2,10 +2,18 @@ import { beforeEach, expect, test, vi } from 'vitest'
 import {
   window, env,
   // @ts-expect-error mock feature
-  terminal
+  terminal,
+  NotebookDocument,
+  TextDocument,
+  ViewColumn,  
 } from 'vscode'
 
-import { openTerminal, copyCellToClipboard, runCLICommand } from '../../../src/extension/commands'
+import { 
+  openTerminal,
+  copyCellToClipboard, 
+  runCLICommand, 
+  openAsRunmeNotebook, 
+  openSplitViewAsMarkdownText } from '../../../src/extension/commands'
 import { getTerminalByCell } from '../../../src/extension/utils'
 import { CliProvider } from '../../../src/extension/provider/cli'
 
@@ -59,4 +67,14 @@ test('runCLICommand if CLI is installed', async () => {
   expect(window.createTerminal).toBeCalledWith('CLI: foobar')
   expect(terminal.show).toBeCalledTimes(1)
   expect(terminal.sendText).toBeCalledWith('runme run foobar --chdir="/foo"')
+})
+
+test('open markdown as Runme notebook', (file: NotebookDocument) => {
+  openAsRunmeNotebook(file)
+  expect(window.showNotebookDocument).toBeCalledWith(file, { viewColumn: ViewColumn.Beside})
+})
+
+test('open Runme notebook in text editor', (file: TextDocument) => {
+  openSplitViewAsMarkdownText(file)
+  expect(window.showTextDocument).toBeCalledWith(file, {viewColumn: ViewColumn.Beside})
 })
