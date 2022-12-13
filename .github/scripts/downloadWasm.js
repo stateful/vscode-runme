2b31a79eb3e4a3f4e9004b518dfba6d77a5ff409
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2022-12-13 13:24:28 -0500

    Awaiting stable release

--- .github/scripts/downloadWasm.js
+++ .github/scripts/downloadWasm.js
@@ -48,7 +48,11 @@ async function downloadWasm (token) {
   const release = wasmReleaseDefined
     ? releases.data.find((r) => r.tag_name === process.env.RUNME_VERSION)
     : releases.data.filter(
-        (r) => r.prerelease === (process.env.GITHUB_REF_NAME !== "main")
+        (r) => {
+        return true
+        // todo(sebastian): await runme release
+        // return r.prerelease === (process.env.GITHUB_REF_NAME !== "main")
+      }
       )[0]
 
   if (wasmReleaseDefined && !release) {
