4430170beeffba901b5ecc12b0acae138ae723a3
Ruben Restrepo <ruben@stateful.com>
2023-02-17 09:10:48 -0500

    Fix test

--- tests/extension/extension.test.ts
+++ tests/extension/extension.test.ts
@@ -16,7 +16,7 @@ test('initializes all providers', async () => {
   await ext.initialize(context)
   expect(notebooks.registerNotebookCellStatusBarItemProvider).toBeCalledTimes(6)
   expect(workspace.registerNotebookSerializer).toBeCalledTimes(1)
-  expect(commands.registerCommand).toBeCalledTimes(12)
+  expect(commands.registerCommand).toBeCalledTimes(14)
   expect(window.registerTreeDataProvider).toBeCalledTimes(1)
   expect(window.registerUriHandler).toBeCalledTimes(1)
 
