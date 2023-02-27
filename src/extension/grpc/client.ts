a649d2c7c8af3944298cff335f5bd6b281120745
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2023-02-27 16:04:34 -0500

    Move client construction into grpc client

--- src/extension/grpc/client.ts
+++ src/extension/grpc/client.ts
@@ -2,5 +2,19 @@
 import { ParserServiceClient } from '@buf/stateful_runme.community_timostamm-protobuf-ts/runme/parser/v1/parser_pb.client'
 // eslint-disable-next-line max-len
 import { RunnerServiceClient } from '@buf/stateful_runme.community_timostamm-protobuf-ts/runme/runner/v1/runner_pb.client'
+import { ChannelCredentials } from '@grpc/grpc-js'
+import { GrpcTransport } from '@protobuf-ts/grpc-transport'
 
-export { ParserServiceClient, RunnerServiceClient }
+import { SERVER_ADDRESS } from '../../constants'
+import { getPortNumber } from '../../utils/configuration'
+
+function initParserClient(): ParserServiceClient {
+  const transport = new GrpcTransport({
+    host: `${SERVER_ADDRESS}:${getPortNumber()}`,
+    channelCredentials: ChannelCredentials.createInsecure(),
+  })
+
+  return new ParserServiceClient(transport)
+}
+
+export { ParserServiceClient, RunnerServiceClient, initParserClient }
