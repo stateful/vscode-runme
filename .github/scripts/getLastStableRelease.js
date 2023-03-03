f156896b083d6a871959198656e1c22af27ec536
Ruben Restrepo <ruben@stateful.com>
2023-03-03 11:10:52 -0500

    New release workflow and update test step to use last stable binary release

---
+++ .github/scripts/getLastStableRelease.js
@@ -0,0 +1,23 @@
+import os from 'node:os'
+
+
+const getLastStableRelease = async () => {
+    /**
+     * The latest release is the most recent non-prerelease, non-draft release
+     */
+    const response = await fetch('https://api.github.com/repos/stateful/runme/releases/latest', {
+        headers: {
+            'Accept': 'application/vnd.github+json'
+        }
+    })
+    const { tag_name } = await response.json()
+    const binary = `${os.platform()}_${os.arch()}.tar.gz`
+    const downloadUrl = `https://download.stateful.com/runme/${tag_name.replace('v','')}/runme_${binary}`
+    // This console log is important since it's being exported to a ENV VAR
+    console.log(downloadUrl)
+}
+
+
+await getLastStableRelease()
+
+
