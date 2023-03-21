5ceaa36f6cb0fca1e4e486f39b0c4823da8cb68b
Max Stoumen <max@mxs.dev>
2023-03-21 10:58:07 -0500

    fix unit tests

--- tests/extension/extension.test.ts
+++ tests/extension/extension.test.ts
@@ -40,7 +40,7 @@ test('initializes all providers', async () => {
   const context: any = { subscriptions: [], extensionUri: { fsPath: '/foo/bar' } }
   const ext = new RunmeExtension()
   await ext.initialize(context)
-  expect(notebooks.registerNotebookCellStatusBarItemProvider).toBeCalledTimes(6)
+  expect(notebooks.registerNotebookCellStatusBarItemProvider).toBeCalledTimes(5)
   expect(workspace.registerNotebookSerializer).toBeCalledTimes(1)
   expect(commands.registerCommand).toBeCalledTimes(14)
   expect(window.registerTreeDataProvider).toBeCalledTimes(1)
