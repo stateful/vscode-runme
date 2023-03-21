c2074b684e3f6aeb91b98a7113aa9e58de0f74be
Max Stoumen <max@mxs.dev>
2023-03-21 13:31:52 -0500

    try to format sync.ts

--- src/utils/sync.ts
+++ src/utils/sync.ts
@@ -7,35 +7,35 @@
   https://github.com/golang/vscode-go/blob/fa820d45d7b9217e949c21ffd13268e20c7e0ee4/src/utils/mutex.ts#L8
 */
 export class Mutex {
-	private mutex = Promise.resolve()
+  private mutex = Promise.resolve()
 
-	private lock(): PromiseLike<() => void> {
-		// Based on https://spin.atomicobject.com/2018/09/10/javascript-concurrency/
+  private lock(): PromiseLike<() => void> {
+    // Based on https://spin.atomicobject.com/2018/09/10/javascript-concurrency/
 
-		let x: (unlock: () => void) => void
+    let x: (unlock: () => void) => void
 
-		// add to the promise chain of this mutex.
-		// When all the prior promises in the chain are resolved,
-		// x, which will be the resolve callback of promise B,
-		// will run and cause to unblock the waiter of promise B.
-		this.mutex = this.mutex.then(() => {
-			return new Promise(x) // promise A
-		})
+    // add to the promise chain of this mutex.
+    // When all the prior promises in the chain are resolved,
+    // x, which will be the resolve callback of promise B,
+    // will run and cause to unblock the waiter of promise B.
+    this.mutex = this.mutex.then(() => {
+      return new Promise(x) // promise A
+    })
 
-		return new Promise((resolve) => {
-			// promise B
-			x = resolve
-		})
-		// the returned Promise will resolve when all the previous
-		// promises chained in this.mutex resolve.
-	}
+    return new Promise((resolve) => {
+      // promise B
+      x = resolve
+    })
+    // the returned Promise will resolve when all the previous
+    // promises chained in this.mutex resolve.
+  }
 
-  public async withLock<T>(cb: () => Promise<T>|T): Promise<T> {
+  public async withLock<T>(cb: () => Promise<T> | T): Promise<T> {
     const unlock = await this.lock()
 
     try {
       return await cb()
-    } catch(e) {
+    } catch (e) {
       throw e
     } finally {
       unlock()
