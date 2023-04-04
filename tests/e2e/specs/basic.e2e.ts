92e0a10a202565de0decba84c4746ab576019bfd
Ruben Restrepo <ruben@stateful.com>
2023-04-04 15:44:08 -0500

    Runme e2e tests

--- tests/e2e/specs/basic.e2e.ts
+++ tests/e2e/specs/basic.e2e.ts
@@ -1,6 +1,72 @@
 import { DefaultTreeItem } from 'wdio-vscode-service'
+import { ElementArray, Key } from 'webdriverio'
 
-describe('Runme VS Code Extension', () => {
+enum OutputType {
+  NotebookTerminal,
+  ShellOutput
+}
+
+const getCell = async (content: string, rows: ElementArray): Promise<WebdriverIO.Element> => {
+  let row: WebdriverIO.Element | undefined
+  for (const r of rows) {
+    const text = await r.getText()
+    if (text.includes(content)) {
+      row = r
+    }
+  }
+  if (!row) {
+    throw new Error(`Could not find cell with content ${content}`)
+  }
+  return row
+}
+
+const executeCell = async (executionCell: WebdriverIO.Element) => {
+  const container = await executionCell.parentElement().parentElement()
+  await container.$('.run-button-container').click()
+}
+
+const switchToCellOutput = async () => {
+  await browser.switchToParentFrame()
+  const iframe = await $$('iframe')[0]
+  await browser.switchToFrame(iframe!)
+  const anotherIframe = await $$('iframe')[0]
+  await browser.switchToFrame(anotherIframe!)
+
+}
+
+const getCellOutput = async () => {
+  const notebookTerminal = await $('terminal-view')
+  const shellOutput = await $('shell-output')
+
+  if (!notebookTerminal.error) {
+    return { outputType: OutputType.NotebookTerminal, outputReference: notebookTerminal }
+  }
+
+  if (!shellOutput.error) {
+    return { outputType: OutputType.ShellOutput, outputReference: shellOutput }
+  }
+
+  throw new Error('Could not found an output')
+}
+
+const resolveCellOuput = async (cell: string): Promise<WebdriverIO.Element | undefined> => {
+  const rows = await $$('.cell-editor-container')
+  const row = await getCell(cell, rows)
+  if (row) {
+    await executeCell(row)
+    await switchToCellOutput()
+    const { outputType, outputReference } = await getCellOutput()
+    switch (outputType) {
+      case OutputType.NotebookTerminal:
+        return outputReference.shadow$('#terminal')
+      case OutputType.ShellOutput:
+        return outputReference
+      default: throw new Error('Invalid cell output')
+    }
+  }
+}
+
+describe('Runme VS Code Extension', async () => {
   it('should load successfully', async () => {
     const workbench = await browser.getWorkbench()
     const title = await workbench.getTitleBar().getTitle()
@@ -51,27 +117,62 @@ describe('Runme VS Code Extension', () => {
     }
   })
 
-  it('should be able to run an example', async () => {
-    const rows = await $$('.cell-editor-container')
-    let row: WebdriverIO.Element | undefined
-    for (const r of rows) {
-      if ((await r.getText()).includes('"Foo 👀')) {
-        row = r
+
+  describe('Runme examples', async () => {
+
+    afterEach(async () => {
+      const handle = await browser.getWindowHandle()
+      return browser.switchToWindow(handle)
+    })
+
+    it('basic hello world shell execution', async () => {
+      const outputReference = await resolveCellOuput('echo "Hello World!')
+      expect(outputReference).toHaveText('Hello World!')
+    })
+
+
+    it('more shell example', async () => {
+      const outputReference = await resolveCellOuput('echo "Foo 👀"\nsleep 2\necho "Bar 🕺"\nsleep 2\necho "Loo 🚀"')
+      expect(outputReference).toHaveText('Foo 👀\nBar 🕺\nLoo 🚀')
+    })
+
+    it('background task example', async () => {
+      const outputReference = await resolveCellOuput('sleep 100000')
+      expect(outputReference).toHaveText('')
+    })
+
+    it('complex output', async () => {
+      const outputReference = await resolveCellOuput('npm i -g webdriverio')
+      if (outputReference) {
+        const regex = new RegExp(/(added|changed) \d+ packages in \d+(?:ms|s)/)
+        let expectedText = ''
+        await browser.waitUntil(async () => {
+          const text = await outputReference.getText()
+          expectedText = text
+          return regex.test(text)
+        }, {
+          timeout: 20000
+        })
+        return expect(outputReference).toHaveText(expectedText)
       }
-    }
+    })
 
-    if (!row) {
-      throw new Error('Could not find cell')
-    }
 
-    const container = await row.parentElement().parentElement()
-    await container.$('.run-button-container').click()
+    it('stdin example', async () => {
+      const outputReference = await resolveCellOuput('node ./scripts/stdin.js')
+      if (outputReference) {
+        const answer1 = 'I love it'
+        const answer2 = 'Great'
+        expect(outputReference).toHaveText('What do you think of Node.js?')
+        await browser.keys([answer1, Key.Enter])
+        await browser.keys([answer2, Key.Enter])
+        expect(outputReference).toHaveText(`
+        What do you think of Node.js? ${answer1}
+        Thank you for your valuable feedback: ${answer1}
+        Another question: how are you today? ${answer2}
+        I am glad you are feeling: ${answer2}`)
+      }
+    })
   })
 
-  it('should expected result in cell', async () => {
-    const workbench = await browser.getWorkbench()
-    const webview = (await workbench.getAllWebviews())[0]
-    await webview.open()
-    await expect($('.output_container').$('>>>pre')).toHaveText('Foo 👀\nBar 🕺\nLoo 🚀')
-  })
 })
