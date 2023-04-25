cb718643c2ce229679176c983bba86b1b79da09e
Max Stoumen <max@mxs.dev>
2023-04-25 13:14:54 -0700

    use serializer types from grpc

--- src/extension/grpc/serializerTypes.ts
+++ src/extension/grpc/serializerTypes.ts
@@ -1,11 +1,1 @@
-import {
-  DeserializeRequest,
-  SerializeRequest,
-  Notebook,
-} from '@buf/stateful_runme.community_timostamm-protobuf-ts/runme/parser/v1/parser_pb'
-
-export {
-  DeserializeRequest,
-  SerializeRequest,
-  Notebook,
-}
+export * from '@buf/stateful_runme.community_timostamm-protobuf-ts/runme/parser/v1/parser_pb'
