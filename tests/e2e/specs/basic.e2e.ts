7b92d5c7a95f1e50590092f8ee4d870853e9f75e
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2023-05-12 09:37:28 -0400

    Match subset of words

--- tests/e2e/specs/basic.e2e.ts
+++ tests/e2e/specs/basic.e2e.ts
@@ -354,5 +354,5 @@ async function tryExecuteCommand(workbench: Workbench, command: string) {
 // }
 
 async function clearAllOutputs(workbench: Workbench) {
-  await tryExecuteCommand(workbench, 'notebook: clear all outputs')
+  await tryExecuteCommand(workbench, 'Notebook: Clear Outputs')
 }
