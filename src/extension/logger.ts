d3bbf82f68afc6b83cc458e627adb85da83fd084
Christian Bromann <git@bromann.dev>
2023-05-18 11:09:05 -0700

    PR feedback

--- src/extension/logger.ts
+++ src/extension/logger.ts
@@ -5,7 +5,7 @@ import { window } from 'vscode'
 const outputChannel = window.createOutputChannel('Runme')
 
 const COLOR_REGEXP = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
-
+const DEFAULT_LOG_LEVEL: LogLevel = 'info'
 
 /**
  * VS Code currently doesn't support colors, see
@@ -18,20 +18,20 @@ const colors = {
   green: '\x1b[32m',
   yellow: '\x1b[33m'
 } as const
+type LogLevel = 'trace' | 'info' | 'warn' | 'error'
 
 function color (color: keyof typeof colors, text: string) {
   return `${colors[color]}${text}${colors.reset}`
 }
 
-function log (scope?: string, ...logParams: string[]) {
+function log (scope?: string, logLevel: LogLevel = DEFAULT_LOG_LEVEL, ...logParams: string[]) {
   const now = new Date()
-  const scopeAddition = scope ? color('yellow', `[${scope}]`) : ''
+  const scopeAddition = scope ? color('yellow', `(${scope})`) : ''
   const prefix = util.format(
-    `${color('green' ,'[%s]')} Runme%s:%s`,
+    `${color('green' ,'[%s]')} ${color('yellow', '%s')} Runme%s:`,
     now.toISOString(),
-    scopeAddition,
-    // if first log param starts with error symbol make sure to include it here where colors have an effect
-    (logParams[0] as string).startsWith(colors.red) ? ` ${logParams.shift()}` : ''
+    logLevel ?? '',
+    scopeAddition
   )
   console.log(prefix, ...logParams)
   outputChannel.appendLine([prefix, ...logParams].join(' ').replace(COLOR_REGEXP, ''))
@@ -39,7 +39,9 @@ function log (scope?: string, ...logParams: string[]) {
 
 export default function getLogger (scope?: string) {
   return {
-    info: (...logParams: unknown[]) => log(scope, ...logParams),
-    error: (...logParams: unknown[]) => log(scope, `${color('red', 'Error')} -`, ...logParams)
+    trace: (...logParams: string[]) => log(scope, 'trace', ...logParams),
+    info: (...logParams: string[]) => log(scope, 'info', ...logParams),
+    warn: (...logParams: string[]) => log(scope, 'warn', ...logParams),
+    error: (...logParams: string[]) => log(scope, 'error', ...logParams)
   }
 }
