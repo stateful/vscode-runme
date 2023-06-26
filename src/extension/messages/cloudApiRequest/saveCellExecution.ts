f366467f664615d46b20b0f95cd84670d4913492
Ruben Restrepo <4001529+degrammer@users.noreply.github.com>
2023-06-26 12:51:44 -0500

    Add Runme authentication (#629)
    * Add Runme authentication

--- src/extension/messages/cloudApiRequest/saveCellExecution.ts
+++ src/extension/messages/cloudApiRequest/saveCellExecution.ts
@@ -1,10 +1,14 @@
-import { ClientMessages } from '../../../constants'
+import { authentication } from 'vscode'
+import { TelemetryReporter } from 'vscode-telemetry'
+
+import { AuthenticationProviders, ClientMessages } from '../../../constants'
 import { ClientMessage, IApiMessage } from '../../../types'
 import { InitializeClient } from '../../api/client'
 import { getCellByUuId } from '../../cell'
 import { getAnnotations, getTerminalByCell } from '../../utils'
 import { createCellExecutionQuery } from '../../api/grapql'
 import { postClientMessage } from '../../../utils/messaging'
+import { RunmeService } from '../../services/runme'
 type APIRequestMessage = IApiMessage<ClientMessage<ClientMessages.cloudApiRequest>>
 
 export default async function saveCellExecution(
@@ -13,6 +17,17 @@ export default async function saveCellExecution(
   const { messaging, message, editor } = requestMessage
 
   try {
+    const session = await authentication.getSession(
+      AuthenticationProviders.GitHub,
+      ['user:email'],
+      {
+        createIfNone: true,
+      }
+    )
+
+    if (!session) {
+      throw new Error('You must authenticate with your GitHub account')
+    }
     const cell = await getCellByUuId({ editor, uuid: message.output.uuid })
     if (!cell) {
       throw new Error('Cell not found')
@@ -30,8 +45,12 @@ export default async function saveCellExecution(
       : 0
     const annotations = getAnnotations(cell)
     delete annotations['runme.dev/uuid']
-
-    const graphClient = InitializeClient()
+    const runmeService = new RunmeService({ githubAccessToken: session.accessToken })
+    const runmeTokenResponse = await runmeService.getAccessToken()
+    if (!runmeTokenResponse) {
+      throw new Error('Unable to retrieve an access token')
+    }
+    const graphClient = InitializeClient({ runmeToken: runmeTokenResponse.token })
     const result = await graphClient.mutate({
       mutation: createCellExecutionQuery({
         ...message.output.data,
@@ -45,12 +64,13 @@ export default async function saveCellExecution(
         },
       }),
     })
-
+    TelemetryReporter.sendTelemetryEvent('runme-app-share')
     return postClientMessage(messaging, ClientMessages.cloudApiResponse, {
       data: result,
       uuid: message.output.uuid,
     })
   } catch (error) {
+    TelemetryReporter.sendTelemetryEvent('runme-app-error')
     return postClientMessage(messaging, ClientMessages.cloudApiResponse, {
       data: (error as any).message,
       uuid: message.output.uuid,
