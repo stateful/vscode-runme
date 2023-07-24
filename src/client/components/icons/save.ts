1390f036b2ca976db297b3af3f8b90784f85e33e
Ruben Restrepo <ruben@stateful.com>
2023-07-24 15:21:16 -0500

    Remove hardcoded white color from save button

--- src/client/components/icons/save.ts
+++ src/client/components/icons/save.ts
@@ -14,7 +14,6 @@ export const SaveIcon = html`
       fill-rule="evenodd"
       clip-rule="evenodd"
       d="M13.353 1.146L14.853 2.646L15 3V14.5L14.5 15H1.5L1 14.5V1.5L1.5 1H13L13.353 1.146ZM2 2V14H14V3.208L12.793 2H11V6H4V2H2ZM8 2V5H10V2H8Z"
-      fill="#C5C5C5"
     />
   </svg>
 `
