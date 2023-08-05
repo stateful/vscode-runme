f457a4f5596003763ac80fe8135a519a53f8f5ab
Christian Bromann <git@bromann.dev>
2023-08-04 19:08:44 -0700

    (test): fix prettier

--- src/extension/executors/github/workflows.ts
+++ src/extension/executors/github/workflows.ts
@@ -57,7 +57,7 @@ export async function getService(createIfNone?: boolean) {
     (await authentication.getSession(
       AuthenticationProviders.GitHub,
       ['repo'],
-      createIfNone ? { createIfNone } : {}
+      createIfNone ? { createIfNone } : {},
     ))
   if (!session) {
     throw new Error('Missing a valid GitHub session')
