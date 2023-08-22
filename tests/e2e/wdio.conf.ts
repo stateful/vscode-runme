645473cec817b15b32ef6e8069924d01a67a9513
Christian Bromann <git@bromann.dev>
2023-08-22 12:05:41 -0700

    bump waitforTimeout

--- tests/e2e/wdio.conf.ts
+++ tests/e2e/wdio.conf.ts
@@ -138,7 +138,7 @@ export const config: Options.Testrunner = {
     baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 30 * 1000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
