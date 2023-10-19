0c9474ede213e21662d7d8048177bc5bf9f1e413
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2023-10-19 15:21:23 -0400

    The best fix is to not prematurely dispose terminal

--- src/extension/kernel.ts
+++ src/extension/kernel.ts
@@ -640,7 +640,6 @@ export class Kernel implements Disposable {
     // Dispose previously attached terminal
     const activeTerminal = this.activeTerminals.find((t) => t.runmeId === runmeId)
     if (activeTerminal) {
-      // activeTerminal.dispose()
       this.activeTerminals.splice(this.activeTerminals.indexOf(activeTerminal), 1)
     }
     const exists = this.activeTerminals.find((t) => t.executionId === executionId)
