7d93397789e8fc6a842db454d476228e2615e4ec
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-01-15 12:43:37 -0300

    Remove scripts

--- scripts/restore-ci.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Restore package-lock.json \\n"
-mv .package-lock.json package-lock.json
-
-echo "Removing node_modules \\n"
-find . -name '*node_modules' -type d -prune -exec rm -rf '{}' +
