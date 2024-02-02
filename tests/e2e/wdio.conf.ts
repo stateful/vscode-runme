089b91826b13b1795ba015de6cc94d88f5ba69e4
Adam Christian <adam.christian@gmail.com>
2024-02-02 06:44:50 -0800

    Use the gif for the new video (#1090)
    * Use the gif for the new video
    
    * Pin prev version until chromedriver is available
    
    ---------
    
    Co-authored-by: Sebastian Tiedtke <sebastiantiedtke@gmail.com>

--- tests/e2e/wdio.conf.ts
+++ tests/e2e/wdio.conf.ts
@@ -93,7 +93,7 @@ export const config: Options.Testrunner = {
   capabilities: [
     {
       browserName: 'vscode',
-      browserVersion: 'stable',
+      browserVersion: '1.85.2',
       'wdio:vscodeOptions': {
         extensionPath,
         workspacePath: extensionPath,
