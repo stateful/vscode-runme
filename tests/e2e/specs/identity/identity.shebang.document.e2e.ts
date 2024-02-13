cb9baa7faeb528c2f3398f44f600b92c4536c000
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-02-13 11:06:29 -0500

    Test against v3

--- tests/e2e/specs/identity/identity.shebang.document.e2e.ts
+++ tests/e2e/specs/identity/identity.shebang.document.e2e.ts
@@ -62,7 +62,7 @@ describe('Test suite: Shebang with setting Document only (2)', async () => {
       `---
       runme:
         id: 01HEXJ9KWG7BYSFYCNKSRE4JZR
-        version: v2.2
+        version: v3
       ---
 
       ## Shebang
