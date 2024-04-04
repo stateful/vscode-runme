5795c12dc9bcbe02519c721ac68db9a9f42e46c0
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-04-04 10:14:10 -0400

    Fix test case

--- tests/e2e/specs/identity/identity.existent-cell.none.e2e.ts
+++ tests/e2e/specs/identity/identity.existent-cell.none.e2e.ts
@@ -65,7 +65,7 @@ describe('Test suite: Cell with existent identity and setting None (0)', async (
 
       ## Scenario
 
-      \`\`\`js {"id":"01HER3GA0RQKJETKK5X5PPRTB4"}
+      \`\`\`js
       console.log("Hello via Shebang")
 
       \`\`\`
