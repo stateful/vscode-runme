b127b43445721d63a52d8596d103b19b05792da8
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-04-08 10:44:51 -0400

    Terminal prototype

--- src/extension/kernel.ts
+++ src/extension/kernel.ts
@@ -18,6 +18,7 @@ import {
   NotebookRange,
   NotebookEditorRevealType,
   NotebookEditorSelectionChangeEvent,
+  CancellationToken,
 } from 'vscode'
 import { TelemetryReporter } from 'vscode-telemetry'
 
@@ -58,7 +59,7 @@ import {
   getWorkspaceFolder,
   getRunnerSessionEnvs,
 } from './utils'
-import { isShellLanguage } from './executors/utils'
+import { getSystemShellPath, isShellLanguage } from './executors/utils'
 import './wasm/wasm_exec.js'
 import { RpcError } from './grpc/client'
 import { IRunner, IRunnerReady } from './runner'
@@ -158,6 +159,7 @@ export class Kernel implements Disposable {
       window.onDidChangeActiveNotebookEditor(this.#handleActiveNotebook.bind(this)),
       this.panelManager,
       this.onVarsChangeEvent,
+      this.registerTerminalProfile(),
     )
   }
 
@@ -798,6 +800,37 @@ export class Kernel implements Disposable {
     if (!exists) {
       this.activeTerminals.push({ ...terminal, executionId, runmeId })
     }
+  }
+
+  registerTerminalProfile(): Disposable {
+    const kernel = this
+    const baseUri = workspace.workspaceFolders![0].uri
+    const cwd = baseUri.fsPath
+
+    return window.registerTerminalProfileProvider('runme.terminalProfile', {
+      async provideTerminalProfile(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        token: CancellationToken,
+      ) {
+        const runner = kernel.runner!
+        const sysShell = getSystemShellPath() || '/bin/bash'
+        const program = await runner.createProgramSession({
+          programName: `${sysShell} -l`,
+          tty: true,
+          cwd,
+        })
+
+        program.registerTerminalWindow('vscode')
+        program.setActiveTerminalWindow('vscode')
+
+        return {
+          options: {
+            name: 'Runme Terminal',
+            pty: program,
+          },
+        }
+      },
+    })
   }
 
   getTerminal(runmeId: string) {
