b3968b3939cf933ee0e8e06c5acb0cbc7260b0b7
Jeremy Lewi <jeremy@lewi.us>
2024-05-16 07:43:28 -0700

    Client can generate cells using the agent and insert them.

---
+++ src/extension/ai/converters.ts
@@ -0,0 +1,86 @@
+// Conversion routines between the protocol buffer format and VSCode's representation of a notebook
+//
+// See ../vscode_apis.md for an exlanation. It is very helpful for understanding this folder.
+
+import * as vscode from 'vscode'
+//import * as metadata from './metadata'
+//import * as constants from './constants'
+import {
+  Notebook,
+  Cell,
+  CellKind,
+  CellOutput,
+  CellOutputItem,
+} from '@buf/stateful_runme.community_timostamm-protobuf-ts/runme/parser/v1/parser_pb'
+
+// cellToCellData converts a NotebookCell to a NotebookCellData.
+// NotebookCell is an interface used by the editor.
+// NotebookCellData is a concrete class.
+export function cellToCellData(cell: vscode.NotebookCell): vscode.NotebookCellData {
+  let data = new vscode.NotebookCellData(
+    cell.kind,
+    cell.document.getText(),
+    cell.document.languageId,
+  )
+
+  data.metadata = cell.metadata
+  data.outputs = []
+  for (let o of cell.outputs) {
+    data.outputs.push(o)
+  }
+  return data
+}
+
+// cellDataToProto converts an instance of NotebookCellData to the proto.
+export function cellDataToProto(cell: vscode.NotebookCellData): Cell {
+  let cellPb = Cell.create()
+  cellPb.value = cell.value
+  cellPb.languageId = cell.languageId
+  if (cell.kind === vscode.NotebookCellKind.Code) {
+    cellPb.kind = CellKind.CODE
+  } else {
+    cellPb.kind = CellKind.MARKUP
+  }
+
+  // TODO(jeremy):  cell.metadata is type map[string]Any; cellPb.metadata is type map[string]string
+  // How do we handle the conversion?
+  //cellPb.metadata = cell.metadata
+
+  let cellOutputs: vscode.NotebookCellOutput[] = []
+  if (cell.outputs !== undefined) {
+    cellOutputs = cell.outputs
+  }
+  for (const output of cellOutputs) {
+    let out = CellOutput.create()
+    out.items = []
+    for (const item of output.items) {
+      let outItem = CellOutputItem.create()
+      outItem.data = item.data
+      outItem.mime = item.mime
+      out.items.push(outItem)
+    }
+    cellPb.outputs.push(out)
+  }
+  return cellPb
+}
+
+// protoToCellData converts a cell proto to a NotebookCellData.
+export function protoToCellData(cellPb: Cell): vscode.NotebookCellData {
+  let kind = vscode.NotebookCellKind.Markup
+  if (cellPb.kind === CellKind.CODE) {
+    kind = vscode.NotebookCellKind.Code
+  }
+
+  let newCell = new vscode.NotebookCellData(kind, cellPb.value, cellPb.languageId)
+
+  newCell.metadata = cellPb.metadata
+  newCell.outputs = []
+  for (let output of cellPb.outputs) {
+    let items: vscode.NotebookCellOutputItem[] = []
+    for (let item of output.items) {
+      items.push(new vscode.NotebookCellOutputItem(item.data, item.mime))
+    }
+    newCell.outputs.push(new vscode.NotebookCellOutput(items))
+  }
+  return newCell
+}
--- src/extension/ai/generate.ts
+++ src/extension/ai/generate.ts
@@ -1,61 +1,71 @@
 import * as vscode from 'vscode'
 // import {FoyleClient, getTraceID} from './client';
 // import * as converters from './converters';
-// import * as docpb from "../gen/foyle/v1alpha1/doc_pb";
-// import * as agentpb from "../gen/foyle/v1alpha1/agent_pb";
 // generateCompletion generates a completion by calling the foyle backend and then adds
 // the returned blocks to the window
+import {
+  GenerateCellsRequest,
+  GenerateCellsResponse,
+} from '@buf/stateful_runme.community_timostamm-protobuf-ts/runme/ai/v1alpha1/ai_pb'
+import {
+  Notebook,
+  Cell,
+} from '@buf/stateful_runme.community_timostamm-protobuf-ts/runme/parser/v1/parser_pb'
+import { initAIServiceClient } from './client'
+import * as converters from './converters'
+
 export async function generateCompletion() {
-  vscode.window.showInformationMessage('Hello World From the Foyle AI Extension!')
-  // const editor = vscode.window.activeNotebookEditor;
+  const editor = vscode.window.activeNotebookEditor
 
-  // if (!editor) {
-  //   return;
-  // }
+  if (!editor) {
+    return
+  }
 
-  // if (editor?.selection.isEmpty) {
-  //   return;
-  // }
+  if (editor?.selection.isEmpty) {
+    return
+  }
 
-  // // We subtract 1 because end is non-inclusive
-  // const lastSelectedCell = editor?.selection.end - 1;
-  // var lastActiveCell = editor?.notebook.cellAt(lastSelectedCell);
-  // let cells = editor?.notebook.getCells(new vscode.NotebookRange(0, editor?.notebook.cellCount));
+  // We subtract 1 because end is non-inclusive
+  const lastSelectedCell = editor?.selection.end - 1
+  var lastActiveCell = editor?.notebook.cellAt(lastSelectedCell)
+  console.log('Getting cells')
+  let cells = editor?.notebook.getCells(new vscode.NotebookRange(0, editor?.notebook.cellCount))
+  console.log('Got cells')
 
-  // let doc = new docpb.Doc();
-  // doc.blocks = [];
-  // for (let cell of cells) {
-  //   let block = converters.cellDataToBlock(converters.cellToCellData(cell));
-  //   doc.blocks.push(block);
-  // }
+  const req = GenerateCellsRequest.create()
+  req.notebook = Notebook.create()
+  req.notebook.cells = []
+  for (let cell of cells) {
+    let cellPb = converters.cellDataToProto(converters.cellToCellData(cell))
+    req.notebook.cells.push(cellPb)
+  }
 
-  // let client = new FoyleClient();
+  let client = initAIServiceClient()
 
-  // const request = new agentpb.GenerateRequest();
-  // request.doc = doc;
+  client
+    .generateCells(req)
+    .then((finished) => {
+      let response = finished.response
+      // TODO(jeremy): We should have the server add the traceId to the response and then we should
+      // log it here. This is for debugging purposes as it allows to easily link to the logs
+      //console.log(`Generate request succeeded traceId: ${traceId}`)
 
-  // client.generate(request).then((response: agentpb.GenerateResponse) => {
-  //   var traceId = "";
+      // To add the traceId to the input data we need to create a mutation
+      //const traceIdEdit = createAddTraceIDMutation(lastActiveCell, traceId)
 
-  //   traceId = getTraceID(response.blocks);
+      const insertCells = addAIGeneratedCells(lastSelectedCell + 1, response)
 
-  //   console.log(`Generate request succeeded traceId: ${traceId}`);
-
-  //   // To add the traceId to the input data we need to create a mutation
-  //   const traceIdEdit = createAddTraceIDMutation(lastActiveCell, traceId);
-
-  //   const insertCells = addAIGeneratedCells(lastSelectedCell+1, response);
-
-  //   const edit = new vscode.WorkspaceEdit();
-  //   const notebookUri = editor?.notebook.uri;
-  //   edit.set(notebookUri, [insertCells, traceIdEdit]);
-  //   vscode.workspace.applyEdit(edit).then((result:boolean)=>{
-  //     console.log(`applyedit resolved with ${result}`);
-  //   });
-  // }).catch((error) => {
-  //   console.error(`Generate request failed ${error}`);
-  //   return;
-  // });
+      const edit = new vscode.WorkspaceEdit()
+      const notebookUri = editor?.notebook.uri
+      edit.set(notebookUri, [insertCells])
+      vscode.workspace.applyEdit(edit).then((result: boolean) => {
+        console.log(`applyedit resolved with ${result}`)
+      })
+    })
+    .catch((error) => {
+      console.error(`Generate request failed ${error}`)
+      return
+    })
 }
 
 // // addAIGeneratedCells turns the response from the AI model into a set of cells that can be inserted
@@ -73,3 +83,18 @@ export async function generateCompletion() {
 //    // Now insert the new cells at the end of the notebook
 //    return  vscode.NotebookEdit.insertCells(index, newCellData);
 // }
+
+// addAIGeneratedCells turns the response from the AI model into a set of cells that can be inserted into the notebook.
+// This is done by returning a mutation to add the new cells to the notebook.
+// index is the position in the notebook at which the new the new cells should be inserted.
+function addAIGeneratedCells(index: number, response: GenerateCellsResponse): vscode.NotebookEdit {
+  let newCellData: vscode.NotebookCellData[] = []
+
+  for (let newCell of response.cells) {
+    const data = converters.protoToCellData(newCell)
+    newCellData.push(data)
+  }
+
+  // Now insert the new cells at the end of the notebook
+  return vscode.NotebookEdit.insertCells(index, newCellData)
+}
