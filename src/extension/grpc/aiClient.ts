ebc3096b2df0b7083f6b5bcbb37ea3a7a4e67a0e
Jeremy Lewi <jeremy@lewi.us>
2024-05-17 17:06:59 -0700

    Cleanup; uncomment kernel.ts code.

--- src/extension/grpc/aiClient.ts
+++ src/extension/grpc/aiClient.ts
@@ -1,7 +1,6 @@
 import { AIServiceClient } from '@buf/stateful_runme.community_timostamm-protobuf-ts/runme/ai/v1alpha1/ai_pb.client'
 import { GrpcTransport } from '@protobuf-ts/grpc-transport'
 import { ChannelCredentials } from '@grpc/grpc-js' // Import the missing module
-import * as vscode from 'vscode'
 
 import getLogger from '../logger'
 
@@ -9,10 +8,6 @@ const log = getLogger('AIClient')
 
 // TODO(jeremy): Should we take transport as an argument?
 export function initAIServiceClient(address: string): AIServiceClient {
-  //const config = vscode.workspace.getConfiguration(extName)
-  // Include a default so that address is always well defined
-  //const address = config.get<string>('foyleAddress', 'localhost:9080')
-
   log.info(`Connecting to Foyle GRPC: ${address}`)
   const transport = new GrpcTransport({
     host: address,
