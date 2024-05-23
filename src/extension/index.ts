b0d56b6114616e8e7539e91772a593990bd0de43
Cristian Cepeda <43882+pastuxso@users.noreply.github.com>
2024-05-23 12:48:29 -0400

    Add a warning to avoid conflicts across Stateful extensions

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,4 +1,4 @@
-import { extensions, type ExtensionContext } from 'vscode'
+import { window, extensions, type ExtensionContext, env, Uri } from 'vscode'
 import { TelemetryReporter } from 'vscode-telemetry'
 
 import { RunmeExtension } from './extension'
@@ -16,6 +16,16 @@ export async function activate(context: ExtensionContext) {
 
   if (extensionIdentifier === 'stateful.runme' && pfound) {
     log.warn('Skipping extension activation to avoid conflicts')
+    const message =
+      'For optimal performance, we recommend using only one Stateful extension at a time. ' +
+      'Consider uninstalling Stateful Runme to continue using Stateful Platform.'
+
+    const actionText = 'Go to Stateful Runme'
+    const response = await window.showWarningMessage(message, actionText)
+    if (response === actionText) {
+      await env.openExternal(Uri.parse('vscode:extension/stateful.runme'))
+    }
+
     return
   }
 
