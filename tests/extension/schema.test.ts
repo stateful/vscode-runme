f35dac08484df90f1c19ae2ae7433b0b002fddd3
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-06-24 11:11:54 -0700

    Fix tests

--- tests/extension/schema.test.ts
+++ tests/extension/schema.test.ts
@@ -137,7 +137,11 @@ suite('AnnotationSchema', () => {
       const parseResult = CellAnnotationsSchema.safeParse(input) as SafeParseError<any>
       expect(parseResult.success).toBeFalsy()
       const { fieldErrors } = parseResult.error.flatten()
-      expect(fieldErrors).toEqual({ background: ['expected a boolean value'] })
+      expect(fieldErrors).toEqual({
+        background: ['expected a boolean value'],
+        interactive: ['expected a boolean value'],
+        closeTerminalOnSuccess: ['expected a boolean value'],
+      })
     })
 
     test('Should generate safe default values', () => {
