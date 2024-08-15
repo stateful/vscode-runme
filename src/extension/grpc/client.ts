366d64bbbbb6b66174a32bd2bf33678c60611279
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-08-15 16:13:46 -0700

    Specifically check for v1

--- src/extension/grpc/client.ts
+++ src/extension/grpc/client.ts
@@ -88,10 +88,10 @@ function initProjectClient(transport: GrpcTransport): ProjectServiceClient {
 type ReadyPromise = Promise<void | Error>
 
 function getRunnerServiceClient(transport: RpcTransport): IRunnerServiceClient {
-  if (getServerRunnerVersion() === 'v2alpha1') {
-    return new RunnerServiceClientV2(transport) as any
+  if (getServerRunnerVersion() === 'v1') {
+    return new RunnerServiceClientV1(transport) as any
   }
-  return new RunnerServiceClientV1(transport) as any
+  return new RunnerServiceClientV2(transport) as any
 }
 
 export {
