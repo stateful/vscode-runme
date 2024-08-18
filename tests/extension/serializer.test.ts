788d885e73995749dfd52e31fcc486541911b61d
Jeremy Lewi <jeremy@lewi.us>
2024-08-18 11:54:59 -0700

    Update mocks.

--- tests/extension/serializer.test.ts
+++ tests/extension/serializer.test.ts
@@ -29,6 +29,7 @@ vi.mock('vscode', () => ({
     activeNotebookEditor: undefined,
     showErrorMessage: vi.fn().mockResolvedValue({}),
     createOutputChannel: vi.fn(),
+    createTextEditorDecorationType: vi.fn(),
   },
   Uri: { joinPath: vi.fn().mockReturnValue('/foo/bar') },
   workspace: {
