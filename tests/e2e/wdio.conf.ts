18151a7a1b4f6f6af8fb6fc7b7c4bea065ddb2fd
Cristian Cepeda <43882+pastuxso@users.noreply.github.com>
2024-09-26 19:46:00 -0500

    Rollback wdio config

--- tests/e2e/wdio.conf.ts
+++ tests/e2e/wdio.conf.ts
@@ -163,7 +163,7 @@ export const config: Options.Testrunner = {
   framework: 'mocha',
   //
   // The number of times to retry the entire specfile when it fails as a whole
-  specFileRetries: 0,
+  specFileRetries: 2,
   //
   // Delay in seconds between the spec file retry attempts
   // specFileRetriesDelay: 0,
