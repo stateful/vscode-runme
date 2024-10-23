c4d757c3fc4c973325c7342213069774d5c218bf
hotpocket <githotpocket@gmail.com>
2024-10-23 10:33:08 -0700

    allow use of a generic serializer

--- src/extension/kernel.ts
+++ src/extension/kernel.ts
@@ -256,7 +256,7 @@ export class Kernel implements Disposable {
     this.category = category
   }
 
-  setSerializer(serializer: GrpcSerializer) {
+  setSerializer(serializer: SerializerBase) {
     this.serializer = serializer
   }
 
