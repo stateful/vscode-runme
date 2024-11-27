c2bb6377d175e3be8c273fa048fa82e7d3cd1be2
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2024-11-27 11:23:28 -0800

    Separate out feature flag

--- src/extension/features/autoOpenTerminal.ts
+++ src/extension/features/autoOpenTerminal.ts
@@ -6,12 +6,12 @@ import ContextState from '../contextState'
 import { isOnInContextState } from '.'
 
 export async function autoOpenTerminal() {
-  if (!isOnInContextState(FeatureName.HostedPlayground)) {
+  if (!isOnInContextState(FeatureName.OpenTerminalOnStartup)) {
     return
   }
 
-  if (!ContextState.getKey<boolean>(`${FeatureName.HostedPlayground}.autoOpenTerminal`)) {
+  if (!ContextState.getKey<boolean>(`${FeatureName.OpenTerminalOnStartup}.autoOpenTerminal`)) {
     await commands.executeCommand('workbench.action.terminal.new')
-    await ContextState.addKey(`${FeatureName.HostedPlayground}.autoOpenTerminal`, true)
+    await ContextState.addKey(`${FeatureName.OpenTerminalOnStartup}.autoOpenTerminal`, true)
   }
 }
