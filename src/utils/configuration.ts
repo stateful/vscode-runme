1a55850b9bff93ed9f973ec77008565e937b8a14
Cristian Cepeda <43882+pastuxso@users.noreply.github.com>
2024-12-12 16:23:21 -0500

    Refactor StatefulAuthProvider for static initialization and improved session management (#1822)
    * Work in progress
    
    * Creating singleton
    
    * Singletonization
    
    * Sync repo
    
    * Sync tests
    
    * Reapply "Add sign-out event handling and panel HTML refresh for CloudPanel (#1789)"
    
    This reverts commit 0f760253d8cd7cf8f37d15408c237a7f9671ac8e.
    
    * Add AuthSessionChangeHandler to prevent multiple listener calls
    
    * Passing tests
    
    * Remove internal usage of the context from the AuthSessionChangeHandler
    
    * Branch sync
    
    * Removing kernel and RunmeUriHandler dependencies.
    
    * Decrease debounce time
    
    * Silently asks If there is a session for CloudPanel
    
    * No more silence on error
    
    * COde cleanup
    
    * Removes unnecessary code
    
    * Implementing new argument for silent token
    
    * Rollback reactive approach, it’s not working
    
    According to the docs, the value (webview) is not being emmited, it’s because
    the subscription should be settled before the call of the .next() function
    
    I need help on it.
    
    * Test failling
    
    * Rollback panel getAppToken
    
    * Passing tests
    
    * Cleanup
    
    * Minor variable change
    
    * Renamed `getSession` to `currentSession` for better clarity.
    
    - Refactored `ensureSession` to operate as an instance method instead of static.
    - Removes unnecessary code
    
    * Remove token retrieval from `getHydratedHtml` to prevent double authentication
    
    * repository sync
    
    * Remove getPlatformAuthSession function
    
    * Updates tests
    
    * Minor fix
    
    * Emit `signIn` event to update panel when login is triggered externally
    
    * Use cloud over platform
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Sebastian Tiedtke <sebastiantiedtke@gmail.com>

--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -20,9 +20,9 @@ const APP_SECTION_NAME = 'runme.app'
 
 export const OpenViewInEditorAction = z.enum(['split', 'toggle'])
 const DEFAULT_WORKSPACE_FILE_ORDER = ['.env.local', '.env']
-const DEFAULT_RUNME_APP_API_URL = 'https://platform.stateful.com'
-const DEFAULT_RUNME_BASE_DOMAIN = 'platform.stateful.com'
-const DEFAULT_RUNME_REMOTE_DEV = 'staging.platform.stateful.com'
+const DEFAULT_RUNME_APP_API_URL = 'https://cloud.stateful.com'
+const DEFAULT_RUNME_BASE_DOMAIN = 'cloud.stateful.com'
+const DEFAULT_RUNME_REMOTE_DEV = 'staging.cloud.stateful.com'
 const DEFAULT_DOCS_URL = 'https://docs.runme.dev'
 const APP_LOOPBACKS = ['127.0.0.1', 'localhost']
 const APP_LOOPBACK_MAPPING = new Map<string, string>([
