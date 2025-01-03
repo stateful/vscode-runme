52cd4264d475c7b77ec46d3ec9e101726a86c0d2
Cristian Cepeda <43882+pastuxso@users.noreply.github.com>
2025-01-03 11:31:22 -0500

    Bump react and react-dom in /examples/k8s
    Bumps [react](https://github.com/facebook/react/tree/HEAD/packages/react) and [react-dom](https://github.com/facebook/react/tree/HEAD/packages/react-dom). These dependencies needed to be updated together.
    
    Updates `react` from 18.3.1 to 19.0.0
    - [Release notes](https://github.com/facebook/react/releases)
    - [Changelog](https://github.com/facebook/react/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/facebook/react/commits/v19.0.0/packages/react)
    
    Updates `react-dom` from 18.3.1 to 19.0.0
    - [Release notes](https://github.com/facebook/react/releases)
    - [Changelog](https://github.com/facebook/react/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/facebook/react/commits/v19.0.0/packages/react-dom)
    
    ---
    updated-dependencies:
    - dependency-name: react
      dependency-type: direct:production
      update-type: version-update:semver-major
    - dependency-name: react-dom
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- examples/k8s/src/App.test.js
+++ examples/k8s/src/App.test.js
@@ -1,9 +1,10 @@
+import { createRoot } from "react-dom/client";
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = createRoot(div);
+  root.render(<App />);
+  root.unmount();
 });
