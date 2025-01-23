5411104c4e65b0394771de86ee4a67db17d59a60
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-01-23 15:01:06 -0800

    Wrapped GrpcTcpTransport which exclusively supports UDS for now

---
+++ src/extension/grpc/parser/connect/transport.ts
@@ -0,0 +1,41 @@
+import tls from 'node:tls'
+import http2 from 'node:http2'
+import net from 'node:net'
+
+import { Transport } from '@connectrpc/connect'
+import { createGrpcTransport, GrpcTransportOptions } from '@connectrpc/connect-node'
+
+const AUTHORITY = 'https://localhost:0'
+
+export function createGrpcTcpTransport(transportOptions: GrpcTransportOptions): Transport {
+  if (transportOptions.httpVersion !== '2') {
+    throw new Error('This transport only supports HTTP/2')
+  }
+
+  const address = transportOptions.baseUrl
+  if (!address.startsWith('unix://')) {
+    throw new Error('This transport only supports Unix domain sockets')
+  }
+  const path = address.replace('unix://', '')
+
+  let clientSessionOptions: http2.ClientSessionOptions = {
+    createConnection: (_auth, opts) => {
+      const { ca, cert, key } = opts as http2.SecureClientSessionOptions
+
+      // if unset it's not TLS
+      if (!ca || !cert || !key) {
+        return net.connect(path)
+      }
+
+      return tls.connect({ ca, cert, key, path })
+    },
+  }
+
+  transportOptions.nodeOptions = {
+    ...transportOptions.nodeOptions,
+    ...clientSessionOptions,
+  }
+  transportOptions.baseUrl = AUTHORITY
+
+  return createGrpcTransport(transportOptions)
+}
